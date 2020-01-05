1fa15c3787e8984bad393963f7949aad23d45b24
horace <horace@handytoo.com>
2020-01-05 17:21:23 +0800

    增加1特征值和那个特征值，一个预测输出

---
+++ kerasLSTMStock1to1.py
@@ -0,0 +1,189 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  1 21:17:06 2020
+
+@author: 64054
+"""
+import os
+import sys
+#数据预处理以及绘制图形需要的模块
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+##
+from sklearn.preprocessing import MinMaxScaler
+
+from keras.models import Sequential
+
+from keras.layers import Dense, LSTM, BatchNormalization
+###############################################################################
+"""
+从csv文件中读取数据并将数据按照实践从历史到现在处理好
+"""
+def sotck_dataset():
+    #从csv读取数据
+    dataset = pd.read_csv('./000001.csv', encoding = 'gb18030')
+    #print(dataset)
+    #数据集的维度
+    print(dataset.shape)
+    #将开盘价转换为整数
+    dataset['开盘价'] = dataset['开盘价'].astype(int)
+    # 列名选取行
+    dataframeDataset = dataset.loc[1:, ['开盘价']]
+    # 按时间反转下数据
+    dataframeDatasetReverse = dataframeDataset.reindex(index = dataframeDataset.index[::-1])
+    #python list
+    listDataset = dataframeDatasetReverse.values
+    #print(ndarrayDataset)
+    #画图大盘数据图
+    #plt.plot(narrayDataset[:,0], narrayDataset[:,1])
+    #plt.show()
+
+    return dataframeDatasetReverse, listDataset
+
+"""
+归一化函数
+"""
+def sc_fit_transform(nDlist):
+    #将所有数据归一化为0-1的范围
+    sc = MinMaxScaler(feature_range=(0, 1))
+    '''
+    fit_transform()对部分数据先拟合fit，
+    找到该part的整体指标，如均值、方差、最大值最小值等等（根据具体转换的目的），
+    然后对该trainData进行转换transform，从而实现数据的标准化、归一化等等。
+    '''
+    dataset_transform = sc.fit_transform(X=nDlist)
+    #归一化后的数据
+    return sc, np.array(dataset_transform)
+"""
+反归一化函数
+"""
+def sc_inverse_transform(sc, nDlist):
+    return scTestDataseY.inverse_transform(X=nDlist)
+###############################################################################
+#需要之前5次的股票数据来预测下一次的数据，
+need_num = 5
+#训练数据的大小
+training_num = 240
+#测试数据的大小
+test_num = 1
+#迭代训练10次
+epoch = 10
+#每次取数据数量
+batch_size = 10
+###############################################################################
+#数据处理
+# 获取数据
+dataframeDataset, listDataset = sotck_dataset()
+# 生成训练集开盘价巡数据集
+xTrainDataset = []
+xTrainDataset = listDataset[0:training_num]
+# 每次的下次开盘价是训练结果
+yTrainDataset = []
+yTrainDataset = listDataset[1:training_num+1]
+#print(xTrainDataset)
+#print(yTrainDataset)
+###############################################################################
+#原始数据归一化
+#转换位n行1列的二维数组
+xTrainDataset = np.array(xTrainDataset)
+xTrainDataset = xTrainDataset.reshape(-1,1)
+scTrainDataseX, xTrainDataset  = sc_fit_transform(xTrainDataset)
+
+yTrainDataset = np.array(yTrainDataset)
+yTrainDataset = yTrainDataset.reshape(-1,1)
+scTrainDataseY, yTrainDataset  = sc_fit_transform(yTrainDataset)
+print(xTrainDataset.shape)
+print(yTrainDataset.shape)
+
+###############################################################################
+# 生成lstm模型需要的训练集数据和
+xTrain = []
+for i in range(need_num, training_num):
+    xTrain.append(xTrainDataset[i-need_num:i])
+xTrain = np.array(xTrain)
+#print(xTrain)
+print(xTrain.shape)
+#因为LSTM要求输入的数据格式为三维的，[training_number, time_steps, 1]，因此对数据进行相应转化
+xTrain = np.reshape(xTrain, (xTrain.shape[0], xTrain.shape[1], 1))
+#print(xTrain)
+print(xTrain.shape)
+
+yTrain = []
+for i in range(need_num, training_num):
+    yTrain.append(yTrainDataset[i-need_num])
+yTrain = np.array(yTrain)
+#print(yTrain)
+print(yTrain.shape)
+###############################################################################
+#构建网络，使用的是序贯模型
+model = Sequential()
+#return_sequences=True返回的是全部输出，LSTM做第一层时，需要指定输入shape
+model.add(LSTM(units=128, return_sequences=True, input_shape=[xTrain.shape[1], 1]))
+model.add(BatchNormalization())
+ 
+model.add(LSTM(units=128))
+model.add(BatchNormalization())
+ 
+model.add(Dense(units=1))
+#进行配置
+model.compile(optimizer='adam',
+              loss='mean_squared_error',
+              metrics=['accuracy'])
+
+model.fit(x=xTrain, y=yTrain, epochs=epoch, batch_size=batch_size)
+###############################################################################
+#进行测试数据的处理
+#生成训练集开盘价巡数据集
+xTestDataset = []
+xTestDataset = listDataset[test_num:training_num+test_num]
+# 每次的下次开盘价是训练结果
+yTestDataset = []
+yTestDataset = listDataset[test_num+1:training_num+test_num+1]
+#测试集数据归一化
+xTestDataset = np.array(xTestDataset)
+xTestDataset = xTestDataset.reshape(-1,1)
+scTesDatasetX, xTestDataset  = sc_fit_transform(xTestDataset)
+
+yTestDataset = np.array(yTestDataset)
+yTestDataset = yTestDataset.reshape(-1,1)
+scTestDataseY, yTestDataset  = sc_fit_transform(yTestDataset)
+#print(xTestDataset.shape)
+#print(yTestDataset.shape)
+#因为LSTM要求输入的数据格式为三维的，[training_number, time_steps, 1]，因此对数据进行相应转化
+# 生成lstm模型需要的训练集数据和
+xTest = []
+for i in range(need_num, training_num):
+    xTest.append(xTestDataset[i-need_num:i])
+xTest = np.array(xTest)
+#print(xTrain)
+print(xTest.shape)
+#因为LSTM要求输入的数据格式为三维的，[training_number, time_steps, 1]，因此对数据进行相应转化
+xTest = np.reshape(xTest, (xTest.shape[0], xTest.shape[1], 1))
+#print(xTrain)
+print(xTest.shape)
+
+yTest = []
+for i in range(need_num, training_num):
+    yTest.append(yTestDataset[i-need_num])
+yTest = sc_inverse_transform(scTestDataseY, yTest)
+#print(yTrain)
+print(yTest.shape)
+###############################################################################
+#进行预测
+yPredictes = model.predict(x=xTest)
+#print(yPredictes)
+#使用 sc.inverse_transform()将归一化的数据转换回原始的数据，以便我们在图上进行查看
+yPredictes = scTestDataseY.inverse_transform(X=yPredictes)
+############################################################################### 
+#对比结果，绘制数据图表，红色是真实数据，蓝色是预测数据
+plt.plot(yTest, color='red', label='Real Stock Price')
+plt.plot(yPredictes, color='blue', label='Predicted Stock Price')
+plt.title(label='ShangHai Stock Price Prediction')
+plt.xlabel(xlabel='Time')
+plt.ylabel(ylabel='ShangHai Stock Price')
+plt.legend()
+plt.show()
+
+
+
---
+++ kerasLSTMStockNto1.py
@@ -0,0 +1,194 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  1 21:17:06 2020
+
+@author: 64054
+"""
+import os
+import sys
+#数据预处理以及绘制图形需要的模块
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+##
+from sklearn.preprocessing import MinMaxScaler
+
+from keras.models import Sequential
+
+from keras.layers import Dense, LSTM, BatchNormalization
+###############################################################################
+def get_dataset():
+    #从csv读取数据
+    dataset = pd.read_csv('./000001.csv', encoding = 'gb18030')
+    #print(dataset)
+    #数据集的维度
+    print(dataset.shape)
+    #将数据价转换为整数
+    dataset['开盘价'] = dataset['开盘价'].astype(int)
+    dataset['收盘价'] = dataset['收盘价'].astype(int)
+    dataset['成交量'] = dataset['成交量'].astype(int)
+    # 列名选取行，剔除不需要开头的行
+    dataframeDataset = dataset.loc[1:, ['开盘价', '收盘价', '成交量']]
+    print(dataframeDataset.shape)
+    # 反转数据
+    dataframeDatasetReverse = dataframeDataset.reindex(index = dataframeDataset.index[::-1])
+    # 历史日期在前
+    ndarrayDataset = dataframeDatasetReverse.values
+    #print(ndarrayDataset)
+    #画图大盘数据图
+    #plt.plot(ndarrayDataset[:,0], ndarrayDataset[:,1])
+    #plt.show()
+    return ndarrayDataset
+
+def get_lstm_dataset(dataset, need_num, total_dataset):
+    lstm_dataset = []
+    for i in range(need_num, total_dataset):
+        lstm_dataset.append(dataset[i-need_num:i])
+    lstm_dataset = np.array(lstm_dataset)
+    #print(xTrain)
+    #print(lstm_dataset.shape)
+    return lstm_dataset
+
+###############################################################################
+#需要之前5次的股票数据来预测下一次的数据，
+need_num = 5
+#训练数据的大小
+training_num = 240
+#测试数据的大小
+test_num = 1
+#迭代训练10次
+epoch = 10
+#每次取数据数量
+batch_size = 10
+###############################################################################
+#数据处理
+ndarrayDataset = get_dataset()
+###############################################################################
+# 构建训练集
+x_train = ndarrayDataset[0:training_num]
+#print(x_train)
+y_train = []
+for i in range(1, training_num+1):
+    y_train.append(ndarrayDataset[i,0])
+#print(y_train)
+# 转换dt数据
+x_train, y_train = np.array(x_train), np.array(y_train)
+print(x_train.shape)
+print(y_train.shape)
+###############################################################################
+#数据归一化,fit_transform处理后就变成list
+sc_X = MinMaxScaler(feature_range=(0, 1))
+x_train = sc_X.fit_transform(x_train)
+x_train = np.array(x_train)
+print(x_train.shape)
+
+#print(x_train)
+#print(x_train.shape)
+# outcome scaling:
+sc_Y = MinMaxScaler(feature_range=(0, 1))
+y_train = sc_Y.fit_transform(y_train.reshape(-1,1))
+y_train = np.array(y_train)
+print(y_train.shape)
+
+###############################################################################
+#构建LSTM需要的数据
+xTrain = []
+for i in range(need_num, training_num):
+    xTrain.append(x_train[i-need_num:i])
+xTrain = np.array(xTrain)
+#print(xTrain)
+print(xTrain.shape)
+x_train = xTrain
+
+yTrain = []
+for i in range(need_num, training_num):
+    yTrain.append(y_train[i-need_num])
+yTrain = np.array(yTrain)
+#print(yTrain)
+print(yTrain.shape)
+y_train = yTrain
+#因为LSTM要求输入的数据格式为三维的，[training_number, time_steps, 1]，因此对数据进行相应转化
+#x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+###############################################################################
+#构建网络，使用的是序贯模型
+model = Sequential()
+#return_sequences=True返回的是全部输出，LSTM做第一层时，需要指定输入shape
+model.add(LSTM(units=128, return_sequences=True, 
+               input_shape=[x_train.shape[1],
+                            x_train.shape[2]]))
+model.add(BatchNormalization())
+ 
+model.add(LSTM(units=128))
+model.add(BatchNormalization())
+ 
+model.add(Dense(units=1))
+#进行配置
+model.compile(optimizer='adam',
+              loss='mean_squared_error',
+              metrics=['accuracy'])
+###############################################################################
+#训练网络
+model.fit(x=x_train, y=y_train, epochs=epoch, batch_size=batch_size)
+###############################################################################
+#进行测试数据的处理
+#将训练集的数据向后移动，增加上需要预测的数据
+x_test = ndarrayDataset[test_num:training_num+test_num]
+y_test = []
+for i in range(test_num+1, training_num+test_num+1):
+    y_test.append(ndarrayDataset[i,0])
+#print(y_train)
+# 转换dt数据
+x_test, y_test = np.array(x_test), np.array(y_test)
+print(x_test.shape)
+print(y_test.shape)
+###############################################################################
+#测试数据的归一化
+sc_X_T = MinMaxScaler(feature_range=(0, 1))
+x_test = sc_X_T.fit_transform(x_test)
+x_test = np.array(x_test)
+#print(x_test)
+print(x_test.shape)
+sc_Y_T = MinMaxScaler(feature_range=(0, 1))
+y_test_transform = sc_Y_T.fit_transform(y_test.reshape(-1,1))
+y_test_transform = np.array(y_test_transform)
+#print(y_test_transform)
+print(y_test_transform.shape)
+#获取对应的开盘价列
+###############################################################################
+#构建LSTM需要的数据
+xTest = []
+for i in range(need_num, training_num):
+    xTest.append(x_test[i-need_num:i])
+xTest = np.array(xTest)
+#print(xTrain)
+print(xTest.shape)
+x_test = xTest
+
+yTest = []
+for i in range(need_num, training_num):
+    yTest.append(y_test[i-need_num])
+yTest = np.array(yTest)
+#print(yTrain)
+print(yTest.shape)
+y_test = yTest
+###############################################################################
+#进行预测
+y_predictes = model.predict(x=x_test)
+print(y_predictes)
+print(y_predictes.shape)
+#使用 sc.inverse_transform()将归一化的数据转换回原始的数据，以便我们在图上进行查看
+y_predictes = sc_Y_T.inverse_transform(X=y_predictes)
+print(y_predictes)
+###############################################################################
+#绘制数据图表，红色是真实数据，蓝色是预测数据
+plt.plot(y_test, color='red', label='Real Stock Price')
+plt.plot(y_predictes, color='blue', label='Predicted Stock Price')
+plt.title(label='ShangHai Stock Price Prediction')
+plt.xlabel(xlabel='Time')
+plt.ylabel(ylabel='ShangHai Stock Price')
+plt.legend()
+plt.show()
+
+
+
